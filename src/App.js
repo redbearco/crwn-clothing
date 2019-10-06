1ea765480355df7d5d265a6bb6480eef66897586
redbear <rbear.co@gmail.com>
2019-10-06 16:29:27 +0330

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,50 +4,56 @@ import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HomePage2 = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <Link to='/topics'>topics list</Link>
-      <button onClick={() => props.history.push('./topics')}>
-        topics
-      </button>
-      <h1>HatsPage</h1>
-    </div>
-  );
-};
-
-const TopicsList = props => {
-  return (
-    <div>
-      <h1>Topics List Page</h1>
-      <Link to={`${props.match.url}/16`}>To Topic 16</Link>
-      <Link to={`${props.match.url}/15`}>To Topic 15</Link>
-      <Link to={`${props.match.url}/14`}>To Topic 14</Link>
-      <Link to={`${props.match.url}/12`}>To Topic 12</Link>
-    </div>
-  );
-};
-
-const TopicsDetail = (props) => {
-  return (
-    <div>
-      <h1>Topics Detail Page: { props.match.params.topicId }</h1>
-    </div>
-  );
-}
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return ( 
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/topics' component={TopicsList} />
-        <Route exact path='/topics/:topicId' component={TopicsDetail} />
+        <Route exact path='/shop' component={ShopPage} />
+        {/* <Route exact path='/topics' component={TopicsList} />
+        <Route exact path='/topics/:topicId' component={TopicsDetail} /> */}
       </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// const HomePage2 = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <Link to='/topics'>topics list</Link>
+//       <button onClick={() => props.history.push('./topics')}>
+//         topics
+//       </button>
+//       <h1>HatsPage</h1>
+//     </div>
+//   );
+// };
+
+// const TopicsList = props => {
+//   return (
+//     <div>
+//       <h1>Topics List Page</h1>
+//       <Link to={`${props.match.url}/16`}>To Topic 16</Link>
+//       <Link to={`${props.match.url}/15`}>To Topic 15</Link>
+//       <Link to={`${props.match.url}/14`}>To Topic 14</Link>
+//       <Link to={`${props.match.url}/12`}>To Topic 12</Link>
+//     </div>
+//   );
+// };
+
+// const TopicsDetail = (props) => {
+//   return (
+//     <div>
+//       <h1>Topics Detail Page: { props.match.params.topicId }</h1>
+//     </div>
+//   );
+// }
